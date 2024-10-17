a1c550383415c67e9f9103630856f3b0bc6c093b
rohith359 <srohith2584@gmail.com>
2024-10-17 12:26:22 +0530

    sr6.py

---
+++ sr6.py
@@ -0,0 +1,8 @@
+...
+n = int(input())
+tuple_elements = tuple(int(input()) for _ in range(n))
+print(min(tuple_elements))
+...
+n = int(input())
+tuple_elements = tuple(int(input()) for _ in range(n))
+print(min(tuple_elements))
