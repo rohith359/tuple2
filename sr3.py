355a6466e0535ef3500d25959185d36f5679ede3
rohith359 <srohith2584@gmail.com>
2024-10-17 12:22:20 +0530

    sr3.py

---
+++ sr3.py
@@ -0,0 +1,10 @@
+...
+ Write a program to get the tuple elements in a single line separated by spaces and print the number of elements in the given tuple.
+Sample Input:
+10 20 30
+Sample Output:
+3
+...
+input_data = input()
+tuple_elements = tuple(map(int, input_data.split()))
+print(len(tuple_elements))
